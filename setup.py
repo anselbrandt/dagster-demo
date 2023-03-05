e9b940ee57fbe69b872e4e4e2efb0d180379b92c
ansel brandt <ansel@anselbrandt.com>
2023-03-04 23:14:49 -0500

    Add dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import find_packages, setup
 
-setup(
-    name="dagster_demo",
-    packages=find_packages(exclude=["dagster_demo_tests"]),
-    install_requires=[
-        "dagster",
-        "dagster-cloud"
-    ],
-    extras_require={"dev": ["dagit", "pytest"]},
-)
+if __name__ == "__main__":
+    setup(
+        name="my_dagster_project",
+        packages=find_packages(exclude=["my_dagster_project_tests"]),
+        install_requires=[
+            "dagster",
+            "PyGithub",
+            "matplotlib",
+            "pandas",
+            "nbconvert",
+            "nbformat",
+            "ipykernel",
+            "jupytext",
+        ],
+        extras_require={"dev": ["dagit", "pytest"]},
+    )
